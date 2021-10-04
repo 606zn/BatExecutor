935d671571dbd05f03169e6b5d1853ffbc172614
606zn <59501607+606zn@users.noreply.github.com>
2021-10-04 18:29:15 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace BatExecutor
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.batbox = new System.Windows.Forms.TextBox();
+            this.Execute = new System.Windows.Forms.Button();
+            this.Openbat = new System.Windows.Forms.Button();
+            this.Savebat = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // batbox
+            // 
+            this.batbox.Location = new System.Drawing.Point(13, 13);
+            this.batbox.Multiline = true;
+            this.batbox.Name = "batbox";
+            this.batbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.batbox.Size = new System.Drawing.Size(491, 290);
+            this.batbox.TabIndex = 5;
+            this.batbox.TextChanged += new System.EventHandler(this.batbox_TextChanged);
+            // 
+            // Execute
+            // 
+            this.Execute.Location = new System.Drawing.Point(13, 309);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(75, 23);
+            this.Execute.TabIndex = 1;
+            this.Execute.Text = "Execute";
+            this.Execute.UseVisualStyleBackColor = true;
+            this.Execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // Openbat
+            // 
+            this.Openbat.Location = new System.Drawing.Point(147, 309);
+            this.Openbat.Name = "Openbat";
+            this.Openbat.Size = new System.Drawing.Size(75, 23);
+            this.Openbat.TabIndex = 2;
+            this.Openbat.Text = "Openbat";
+            this.Openbat.UseVisualStyleBackColor = true;
+            this.Openbat.Click += new System.EventHandler(this.Openbat_Click);
+            // 
+            // Savebat
+            // 
+            this.Savebat.Location = new System.Drawing.Point(285, 309);
+            this.Savebat.Name = "Savebat";
+            this.Savebat.Size = new System.Drawing.Size(75, 23);
+            this.Savebat.TabIndex = 3;
+            this.Savebat.Text = "Savebat";
+            this.Savebat.UseVisualStyleBackColor = true;
+            this.Savebat.Click += new System.EventHandler(this.Savebat_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(429, 309);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 4;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 339);
+            this.ControlBox = false;
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Savebat);
+            this.Controls.Add(this.Openbat);
+            this.Controls.Add(this.Execute);
+            this.Controls.Add(this.batbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "BatExecutor v0.01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox batbox;
+        private System.Windows.Forms.Button Execute;
+        private System.Windows.Forms.Button Openbat;
+        private System.Windows.Forms.Button Savebat;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatExecutor
+{
+    public partial class Main : Form
+    {
+        /* BatExecutor v 0.01
+         * VS 2019, Net framework 4.5, WinForms
+         */
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            if (batbox.Text != "")
+            {
+                File.WriteAllText(Application.StartupPath + "\\BatExecutor.bat", batbox.Text);
+                Process.Start(Application.StartupPath + "\\BatExecutor.bat");
+            }
+
+            else
+            {
+                MessageBox.Show("Нет скрипта для выполнения.","Error",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void Openbat_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "bat files | *.bat",
+                InitialDirectory = Application.StartupPath
+            };
+            ofd.ShowDialog();
+            string pth = ofd.FileName;
+            if (pth != "")
+            {
+                new FileInfo(pth);
+                StreamReader sr = new StreamReader(pth, System.Text.Encoding.Default);
+                batbox.Text = "";
+                while (!sr.EndOfStream)
+                {
+                    string text = sr.ReadLine() + Environment.NewLine;
+                    batbox.AppendText(text);
+                }
+                sr.Close();
+            }
+
+        }
+
+        private void Savebat_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "bat files | *.bat";
+            sfd.ShowDialog();
+            if (sfd.FileName != "")
+            {
+                File.WriteAllText(sfd.FileName, batbox.Text);
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        private void batbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
